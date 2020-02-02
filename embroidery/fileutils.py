9b958a79967f0346d0fbd743903eba6a34e4bfd1
Gustavo Barbosa <gustavocsb@gmail.com>
2020-02-02 10:34:32 -0500

    fix typo

--- embroidery/fileutils.py
+++ embroidery/fileutils.py
@@ -1,4 +1,4 @@
 def default_output(input_path):
     parts = input_path.split(".")
-    parts[-2] = parts[-2] + "_embroiled"
+    parts[-2] = parts[-2] + "_embroidered"
     return ".".join(parts)
--- tests/test_fileutils.py
+++ tests/test_fileutils.py
@@ -5,4 +5,4 @@ class TestFileUtils:
     class TestDefaultOutput:
         def test_it_appends_embroiled(self):
             output = default_output("/Users/me/Documents/icon.png")
-            assert output == "/Users/me/Documents/icon_embroiled.png"
+            assert output == "/Users/me/Documents/icon_embroidered.png"
