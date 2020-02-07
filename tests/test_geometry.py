c103ffbeaa809d19352e691fad7bd26b42499dcf
Gustavo Barbosa <gustavocsb@gmail.com>
2020-02-07 17:49:02 -0500

    add more tests to geometry

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -11,6 +11,16 @@ IMAGE_SIZE = (100, 100)
 
 
 class TestGeometry:
+    class TestThickness:
+        def test_it_has_correct_value(self):
+            geometry = Geometry(IMAGE_SIZE, TOP_RIGHT)
+            assert geometry.thickness == 25.0
+
+    class TestSize:
+        def test_it_has_correct_vertical_and_horizontal_values(self):
+            geometry = Geometry(IMAGE_SIZE, TOP_RIGHT)
+            assert geometry.size == "62.5x62.5"
+
     class TestPath:
         def test_top_left(self):
             geometry = Geometry(IMAGE_SIZE, TOP_LEFT)
@@ -60,3 +70,33 @@ class TestGeometry:
                 assert False
             except InvalidEmbroideryPosition:
                 pass
+
+    class TestRotation:
+        def test_top_left(self):
+            geometry = Geometry(IMAGE_SIZE, TOP_LEFT)
+            assert geometry.rotation == -45
+
+        def test_top_right(self):
+            geometry = Geometry(IMAGE_SIZE, TOP_RIGHT)
+            assert geometry.rotation == 45
+
+        def test_bottom_left(self):
+            geometry = Geometry(IMAGE_SIZE, BOTTOM_LEFT)
+            assert geometry.rotation == 45
+
+        def test_bottom_right(self):
+            geometry = Geometry(IMAGE_SIZE, BOTTOM_RIGHT)
+            assert geometry.rotation == -45
+
+        def test_invalid_position(self):
+            geometry = Geometry(IMAGE_SIZE, "INVALID")
+            try:
+                geometry.rotation == -45
+                assert False
+            except InvalidEmbroideryPosition:
+                pass
+
+    class TestPointSize:
+        def test_it_has_correct_value(self):
+            geometry = Geometry(IMAGE_SIZE, TOP_RIGHT)
+            assert geometry.pointsize == 12.5
